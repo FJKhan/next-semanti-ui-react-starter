e5c38716e58e80d8699d5e192d40c67b698f5dac
fjkhan <fjgenus@gmail.com>
2019-05-18 01:27:07 -0500

    Adds semantic ui, semantic ui react, and related dependencies. Adds next config. Updates index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import '../static/css/index.css'
+import {
+    Container,
+    Grid,
+    Header,
+    Icon
+} from 'semantic-ui-react'
 
 function Home() {
-    return <div>Welcome to the Next.js Semantic Starter Kit!</div>;
+    return (
+        <div>
+            <Container>
+                <Grid centered columns={1}>
+                    <Grid.Column>
+                        <Header as='h1' textAlign='center'>
+                            <Icon name='terminal' />
+                            Welcome to the Next.js Semantic Starter Kit!
+                        </Header>
+                    </Grid.Column>
+                </Grid>
+                <Grid centered columns={1}>
+                    <div className="ball"></div>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export default Home;
