3050e17f88be765eba9fdbb5cb52806be29879a7
FJKhan <fjgenus@gmail.com>
2023-05-01 19:15:08 -0400

    refactor: :recycle: update files in accordance with latest next framework templates and configs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-const withCSS = require('@zeit/next-css');
+const nextConfig = {
+  reactStrictMode: false,
+}
 
-module.exports = withCSS({
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000
-                }
-            }
-        })
-        return config
-    }
-});
+module.exports = nextConfig
